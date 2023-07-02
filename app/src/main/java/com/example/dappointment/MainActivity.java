aa3a79745390990f97090c4df6eae7f92d45cd49
Leeto <boshoabotsoere7@gmail.com>
2023-07-02 16:07:46 +0200

    uploaded complete project

---
+++ app/src/main/java/com/example/dappointment/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.dappointment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1;
+
+    EditText editTextDate;
+    DatePickerDialog.OnDateSetListener onDateSetListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1=(Button)findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        editTextDate = findViewById(R.id.edit_date_view);
+        editTextDate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this,onDateSetListener, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+        onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                String date = dayOfMonth + "/" + month + "/" + year;
+                editTextDate.setText(date);
+            }
+        };
+    }
+}
